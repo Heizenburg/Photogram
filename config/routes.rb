a8cac44fbd03e927e8757adcd698292037759721
Thato Sello <thatokagisosello@gmail.com>
2017-02-11 19:13:51 +0200

    Add user profile

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
  root 'posts#index'  
  resources :posts do  
   resources :comments
- end  
+ end 
+
+ get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
+ get ':user_name',      to: 'profiles#show', as: :profile    
+ patch ':user_name/edit', to: 'profiles#update', as: :update_profile  
  devise_for :users, :controllers => { registrations: 'registrations' }
  
 end
